e1e922a0ade8975db5c6220c3457ae14f749e273
daniel <--local>
2025-05-04 15:49:19 -0700

    Basic images and presentation

---
+++ js/slide.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) slide.classList.add('active');
+    });
+}
+
+function changeSlide(direction) {
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Auto-slide
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
